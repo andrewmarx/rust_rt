5908a9a9202ad64fa4be90a6dedf8940f7293e7c
Andrew Marx <andrewjmarx@gmail.com>
2018-03-19 00:12:05 -0400

    Added nalgebra vector library
    Changed save_image function to use vector
    
    Created basic ray struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+extern crate nalgebra as na;
+
 use std::fs;
 use std::fs::File;
 use std::io::Write;
-
+use na::{Vector3,};
 
 fn main() {
     let nx = 200;
@@ -14,22 +16,37 @@ fn save_image(x: i32, y: i32) {
     let output = String::from("P3\n") + &x.to_string() + " " + &y.to_string() + "\n255\n";
 
     fs::create_dir_all("./out/").expect("Could not create output directory.");
-    let mut f = File::create("./out/test.ppm").expect("Unable to create file");
+    let mut f = File::create("./out/test2.ppm").expect("Unable to create file");
 
     f.write(output.as_bytes()).expect("Could not write file");
 
     for j in {0..y}.rev() {
         for i in 0..x {
-            let r = i as f64 / x as f64;
-            let g = j as f64 / y as f64;
-            let b= 0.2;
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
+            let color = Vector3::new(i as f32 / x as f32, j as f32 / y as f32, 0.2) * 255.99;
 
-            let output = ir.to_string() + " " + &ig.to_string() + " " + &ib.to_string() + "\n";
+            let mut output = String::new();
+            let mut sep = "";
+
+            for val in color.iter() {
+                let v = *val as i32;
+                output.push_str(sep);
+                output.push_str(&v.to_string());
+                sep = " ";
+            }
+            output.push_str("\n");
 
             f.write(output.as_bytes()).expect("Could not write file");
         }
+    }
+}
+
+struct Ray {
+    pos: Vector3<f32>,
+    dir: Vector3<f32>,
+}
+
+impl Ray {
+    fn point_at_param(&self, t: f32) -> Vector3<f32> {
+        self.pos + t * self.dir
     }
 }
